dafa2876a51ba44b2ecbc9dafe50e58b1e4a8925
Hanif Shah <hanifshah812@gmail.com>
2023-08-30 08:05:38 +0500

    turtle_race

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from turtle import Turtle, Screen
 import random
 
-my_turtle_color = ["red", "blue", "green", "black", "purple", "green yellow", "firebrick", "cornflower blue", "medium violet red", "orchid", "medium slate blue", "lavender", "saddle brown", "deep pink", "orange", "salmon", "orange red", "magenta"]
-my_turtle = Turtle()
-my_turtle.shape("turtle")
-my_turtle.width(15)
-my_turtle.speed(10)
-
-
-def draw_random_walk():
-    random_side = [0, 90, 180, 270]
-    my_turtle.backward(30)
-    my_turtle.setheading(random.choice(random_side))
-
-
-for i in range(3 , 200):
-    my_turtle.color(random.choice(my_turtle_color))
-    draw_random_walk()
-
+my_color = ["red", "blue", "green", "black", "purple", "green yellow", "firebrick", "cornflower blue", "medium violet red", "orchid", "medium slate blue", "lavender", "saddle brown", "deep pink", "orange", "salmon", "orange red", "magenta"] 
 my_screen = Screen()
+user_color = my_screen.textinput(title="Get conntest", prompt="Which turle will win the race? Ennter the color")
+my_screen.setup(width=500, height=400)
+y_position = [-125, -75, -25 , 25 , 75, 125]
+turtle_list = []
+
+for i in range(0,5):
+    tim = Turtle(shape="turtle") 
+    tim.penup()    
+    tim.color(my_color[i])
+    tim.goto(x=-230, y=y_position[i])
+    turtle_list.append(tim)
+
+if user_color:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtle_list:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            wining_color = turtle.pencolor()
+            if wining_color == user_color:
+                print(f"You have won! The {wining_color} win the race.")
+            else:
+                print(f"You have lost! The {wining_color} win the race.")
+
+        turtle.forward(random.randint(0,10))
+        
+
 my_screen.exitonclick()
